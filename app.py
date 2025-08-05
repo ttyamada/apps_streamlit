17ea78f018e234c50e366d20ce6b81cf9206bae7
Tsunetomo Yamada <49137028+ttyamada@users.noreply.github.com>
2025-08-05 16:33:22 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import plotly.graph_objects as go
 import numpy as np
 
-st.title("matplotlib の 3D プロット")
+st.title("Plotly でインタラクティブな 3D プロット")
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+# メッシュデータ
+X, Y = np.meshgrid(np.linspace(-2, 2, 50), np.linspace(-2, 2, 50))
+Z = np.sin(np.sqrt(X**2 + Y**2))
 
-# データ作成
-theta = np.linspace(0, 4 * np.pi, 100)
-z = np.linspace(-2, 2, 100)
-r = z**2 + 1
-x = r * np.sin(theta)
-y = r * np.cos(theta)
+# PlotlyのSurfaceプロット
+fig = go.Figure(data=[go.Surface(z=Z, x=X, y=Y)])
 
-ax.plot(x, y, z, label='3D spiral line')
-ax.legend()
+fig.update_layout(title='3D Surface', autosize=True)
 
-st.pyplot(fig)
+st.plotly_chart(fig)
